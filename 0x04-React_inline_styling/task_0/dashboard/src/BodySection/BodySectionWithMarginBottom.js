273eb65762efecf58d4b60c742a89ae2755bf4df
samuearthur <87957433+samuearthur@users.noreply.github.com>
2023-02-07 09:14:10 +0000

    Task_Done

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BodySection from './BodySection';
+import './BodySection.css'
+
+
+export default class BodySectionWithMarginBottom extends React.Component {
+  render() {
+    return (
+      <div className="bodySectionWithMargin" {...this.props}>
+        <BodySection {...this.props}></BodySection>
+      </div>
+    )
+  }
+}
+
